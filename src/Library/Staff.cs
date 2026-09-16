82db94ad5e40e72f60f0509d783383c6c080e122
JulianMM-UI <alexisjulianmalvarezboni@gmail.com>
2026-09-16 09:39:09 -0300

    magicCambios2

--- src/Library/Staff.cs
+++ src/Library/Staff.cs
@@ -1,7 +1,9 @@
-public class Staff
+using System;
+namespace Roleplay
+public class Staff : IMagicItems
 {
-    public int AttackValue { get; set; }
-    public int DefenseValue { get; set; }
+    public int AttackValue {get;}
+    public int DefenseValue {get;}
 
     public Staff(int attack, int defense)
     {
